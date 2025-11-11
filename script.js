4c602d1cbe6331b666b0b5d1b7aa79679fb77dc6
Sunit Mishra <sunitm@contata.in>
2025-11-11 10:11:08 +0530

    initial commit

--- script.js
+++ script.js
@@ -5,54 +5,24 @@ document.addEventListener("click", function() {
     running = true;
 });
 
-
 async function start() {
-    const subtitleContainerEl = document.getElementById("subtitle-container");
-    const subtitleEl = document.getElementById("subtitle");
-    subtitleEl.innerText = subtitle.trim();
+    if(!data) return;
 
-    movingSubtitle();
-    startVideo();
+    for (let i = 0; i < data.length; i++) {
+        let item = data[i];
+        
+    }
 }
 
-async function movingSubtitle() {
+async function showText() {
     const subtitleEl = document.getElementById("subtitle");
-    subtitleEl.style.transform = `translateX(1000px)`;
-    await sleep(3000); // allow time for the browser to register the initial position
-
-    subtitleEl.style.transition = `transform 720s linear`;
-    subtitleEl.style.transform = `translateX(-${subtitleEl.clientWidth - 100}px)`;
+    
 }
 
-async function startVideo() {
+async function showImage() {
     const videoImageEl = document.getElementById("video-image");
     videoImageEl.style.transition = `transform 120s linear`;
     videoImageEl.style.transform = `scale(2) translateX(25%)`;
-
-    await sleep(120000);
-    videoImageEl.style.transition = `transform 120s linear`;
-    videoImageEl.style.transform = `scale(2) translateX(-25%)`;
-    await sleep(120000);
-
-    videoImageEl.setAttribute("src", "./assets/2.jpg");
-    videoImageEl.style.transition = `transform 120s linear`;
-    videoImageEl.style.transform = `scale(2) translateX(25%)`;
-
-    await sleep(120000);
-    videoImageEl.style.transition = `transform 120s linear`;
-    videoImageEl.style.transform = `scale(2) translateX(-25%)`;
-
-    await sleep(120000);
-
-    videoImageEl.setAttribute("src", "./assets/3.png");
-    videoImageEl.style.transition = `transform 120s linear`;
-    videoImageEl.style.transform = `scale(2) translateX(25%)`;
-
-    await sleep(120000);
-    videoImageEl.style.transition = `transform 120s linear`;
-    videoImageEl.style.transform = `scale(2) translateX(-25%)`;
-
-    await sleep(150000);
 }
 
 async function sleep(ms) {
