36860b1f4b844b110b889a42676cd8e7077fc409
Sunit Mishra <sunitm@contata.in>
2025-11-15 13:42:39 +0530

    fine

--- data.js
+++ data.js
@@ -40,35 +40,75 @@ const data = [
         initialTransform: 'scale(1.2)',
         url: "./vs/4.png",
         transform: "scale(1)",
-        transition: "transform 5s linear",
-        stayTime: 5000,
+        transition: "transform 3s linear",
+        stayTime: 3000,
     },
     
     { text: "It was a few hundred meters long" },
     { text: "It was shiny and reddish in color" },
-    { text: "It spun like a tumbling stick in space" },
+
+    {
+        initialTransform: 'scale(1)',
+        url: "./vs/4.png",
+        transform: "scale(1.3)",
+        transition: "transform 2s linear",
+        stayTime: 2000,
+    },
 
     { text: "It moved extremely fast through space" },
     { text: "It was too fast for our Solar System" },
+
+    {
+        initialTransform: 'scale(1.5)',
+        url: "./vs/5.png",
+        transform: "scale(1)",
+        transition: "transform 3s linear",
+        stayTime: 3000,
+    },
+    
     { text: "It had a strange push without a tail" },
     { text: "The push confused some scientists" },
 
-    { text: "It might be icy under its surface" },
-    { text: "Some invisible gases may have escaped" },
-    { text: "It may be a frozen nitrogen shard" },
-    { text: "It might be a hydrogen iceberg" },
-
     { text: "Some thought it could be alien tech" },
-    { text: "There is no proof for alien ideas" },
-    { text: "Natural explanations fit better" },
-    { text: "Scientists still debate the object" },
+
+    {
+        initialTransform: 'scale(1)',
+        url: "./vs/5.png",
+        transform: "scale(1.5)",
+        transition: "transform 3s linear",
+        stayTime: 3000,
+    },
 
     { text: "ʻOumuamua is leaving the Solar System" },
+
+    {
+        initialTransform: 'scale(1)',
+        url: "./vs/7.png",
+        transform: "scale(1.5)",
+        transition: "transform 3s linear",
+        stayTime: 3000,
+    },
+    
     { text: "We cannot chase it with current tech" },
     { text: "We saw it only for a short time" },
 
+    {
+        initialTransform: 'scale(1.5)',
+        url: "./vs/7.png",
+        transform: "scale(1)",
+        transition: "transform 3s linear",
+        stayTime: 3000,
+    },
+
     { text: "It proved interstellar objects exist" },
-    { text: "It started a new astronomy field" },
     { text: "It may teach us about other stars" },
-    { text: "It changed how we search the sky" }
+
+    {
+        initialTransform: 'scale(1)',
+        url: "./vs/1.jpg",
+        transform: "scale(2)",
+        transition: "transform 10s linear",
+        stayTime: 10000,
+    },
+    
 ]
